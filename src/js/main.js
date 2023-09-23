7330476e06d039293f827b5680d5a471bc3bf8ec
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-09-23 13:42:02 +0200

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 ////////////////////////////////////////zmienne do strony
 
+let $footerYear;
+
 let $btnNavMobile;
 let $navMobile;
 let $navLinksMobile;
@@ -9,11 +11,14 @@ let $navLinksMobile;
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvens();
+	handleCurrentYear();
 };
 
 const prepareDOMElements = () => {
+	$footerYear = document.querySelector('.footer__date__year');
 	$btnNavMobile = document.querySelector('.header__content__btn');
 	$navMobile = document.querySelector('.header__nav');
+	$navLinksMobile = document.querySelectorAll('.header__nav__items__item');
 };
 
 const prepareDOMEvens = () => {
@@ -21,8 +26,20 @@ const prepareDOMEvens = () => {
 };
 ///////////////////////////////////////////////////////Funkcje
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	$footerYear.innerText = year;
+};
+
 const showNav = () => {
 	$navMobile.classList.toggle('active');
+	document.body.classList.toggle('sticky-body');
+	$navLinksMobile.forEach((item) => {
+		item.addEventListener('click', () => {
+			$navMobile.classList.remove('active');
+			document.body.classList.remove('sticky-body');
+		});
+	});
 };
 
 ////////////////////////////////////////////////////////Funkcja Main wyłowanie
