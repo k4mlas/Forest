407898aa9800b0f38ca2864ab646f71137d5fa27
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-10-28 22:36:52 +0200

    form

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,16 @@ let $navLinksMobile;
 let $navLinksDesktop;
 let $sections;
 
+let $name;
+let $mail;
+let $msg;
+let $nameError;
+let $emailError;
+let $textError;
+let $formCheck;
+let $checkError;
+let $formBtn;
+
 ///////////////////////////////////////Funkcja main
 
 const main = () => {
@@ -24,10 +34,20 @@ const prepareDOMElements = () => {
 	$navLinksMobile = document.querySelectorAll('.header__nav__items__item');
 	$navLinksDesktop = document.querySelectorAll('.header__navDesktop__item');
 	$sections = document.querySelectorAll('.sectionScroll');
+	$name = document.getElementById('name');
+	$mail = document.getElementById('mail');
+	$msg = document.getElementById('msg');
+	$nameError = document.getElementById('nameError');
+	$emailError = document.getElementById('emailError');
+	$textError = document.getElementById('textError');
+	$formCheck = document.getElementById('akcept');
+	$checkError = document.getElementById('checkError');
+	$formBtn = document.querySelector('.contact__box__form__btn');
 };
 
 const prepareDOMEvens = () => {
 	$btnNavMobile.addEventListener('click', showNav);
+	$formBtn.addEventListener('click', checkForm);
 };
 ////////////////////////////////////////////////////////////////////////Funkcje
 
@@ -60,16 +80,65 @@ window.onscroll = () => {
 		let height = sec.offsetHeight;
 		let id = sec.getAttribute('id');
 
-
 		if (top >= offset && top < offset + height) {
 			$navLinksDesktop.forEach((links) => {
 				links.classList.remove('activeNav');
-				document.querySelector('.header__navDesktop__item[href*=' + id + ']').classList.add("activeNav")
+				document
+					.querySelector('.header__navDesktop__item[href*=' + id + ']')
+					.classList.add('activeNav');
 			});
 		}
 	});
 };
 
-////////////////////////////////////////////////////////Funkcja Main 
+// Form
+
+const checkForm = () => {
+	checkName();
+	checkEmail();
+	checkText();
+	checkCheck();
+};
+
+const checkName = () => {
+	if ($name.value != '') {
+		$nameError.style.visibility = 'hidden';
+		$name.classList.remove('errorInput');
+	} else {
+		$nameError.style.visibility = 'visible';
+		$name.classList.add('errorInput');
+	}
+};
+
+const checkEmail = () => {
+	const reg =
+		/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if ($mail.value != '' && reg.test($mail.value)) {
+		$emailError.style.visibility = 'hidden';
+		$mail.classList.remove('errorInput');
+	} else {
+		$emailError.style.visibility = 'visible';
+		$mail.classList.add('errorInput');
+	}
+};
+
+const checkText = () => {
+	if ($msg.value != '') {
+		$textError.style.visibility = 'hidden';
+		$msg.classList.remove('errorInput');
+	} else {
+		$textError.style.visibility = 'visible';
+		$msg.classList.add('errorInput');
+	}
+};
+
+const checkCheck = () => {
+	if ($formCheck == false) {
+		$checkError.style.visibility = 'hidden';
+	} else {
+		$checkError.style.visibility = 'visible';
+	}
+};
+////////////////////////////////////////////////////////Funkcja Main
 
 document.addEventListener('DOMContentLoaded', main);
